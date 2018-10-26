79eeddfc524013cbd89080486ccc9dfeb5fd850b
Rayane Ribeiro <rayaneribeiro99@hotmail.com>
2018-10-26 14:13:11 -0400

    Foi criado uma solution, dentro dela foi criadas 3 camadas, a camada web, de aplicação e infraestrutura, e dentro de cada uma foi criadas pastas para separação de dados. Projeto criado na arquitetura Clea DDD.

---
+++ SGL/src/SGL.ApplicationCore/Entity/Lanche.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SGL.ApplicationCore.Entity
+{
+   public class Lanche
+    {
+        public Lanche()
+        {
+
+        }
+        public int LancheId { get; set; }
+
+        public string Nome { get; set; }
+
+        public decimal Valor { get; set; }
+    }
+}
